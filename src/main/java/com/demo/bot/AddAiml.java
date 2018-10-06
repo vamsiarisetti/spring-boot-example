70a40930a3c81c4492efafe0dff05e91fff55de8
vamsiarisetti <32108368+vamsiarisetti@users.noreply.github.com>
2018-10-06 16:43:39 +0530

    Indentation

--- src/main/java/com/demo/bot/AddAiml.java
+++ src/main/java/com/demo/bot/AddAiml.java
@@ -1,39 +1,39 @@
 package com.demo.bot;
- 
+
 import java.io.File;
 import org.alicebot.ab.Bot;
 import org.alicebot.ab.MagicBooleans;
- 
+
 /**
  * @author vamsi
  *
  */
 public class AddAiml {
- 
-    private static final boolean TRACE_MODE = false;
-    static String botName = "super";
- 
-    public static void main(String[] args) {
-        try {
- 
-            String resourcesPath = getResourcesPath();
-            System.out.println(resourcesPath);
-            MagicBooleans.trace_mode = TRACE_MODE;
-            Bot bot = new Bot("super", resourcesPath);
-             
-            bot.writeAIMLFiles();
- 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
- 
-    private static String getResourcesPath() {
-        File currDir = new File(".");
-        String path = currDir.getAbsolutePath();
-        path = path.substring(0, path.length() - 2);
-        System.out.println(path);
-        String resourcesPath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
-        return resourcesPath;
-    }
+
+	private static final boolean TRACE_MODE = false;
+	static String botName = "super";
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("");
+			String resourcesPath = getResourcesPath();
+			System.out.println(resourcesPath);
+			MagicBooleans.trace_mode = TRACE_MODE;
+			Bot bot = new Bot("super", resourcesPath);
+
+			bot.writeAIMLFiles();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String getResourcesPath() {
+		File currDir = new File(".");
+		String path = currDir.getAbsolutePath();
+		path = path.substring(0, path.length() - 2);
+		System.out.println(path);
+		String resourcesPath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
+		return resourcesPath;
+	}
 }
